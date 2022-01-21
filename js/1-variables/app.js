ae89bfa9d10e12384863c6e0571b796797f2af61
Christian Lisangola <jochri3@gmail.com>
2022-01-21 12:59:40 +0300

    JS intro

---
+++ js/1-variables/app.js
@@ -0,0 +1,17 @@
+//Avant let et const
+var pays = 'France'
+pays = 4
+pays = true
+
+if (true) {
+    var nom = 'Christian Lisangola'
+    console.log('dans IF : ', nom)
+}
+console.log('En dehors de IF : ', nom)
+
+for (var i = 1; i <= 5; i++) {
+    console.log('i dans la boucle : ', i)
+}
+console.log('i en dehors de la boucle : ', i)
+
+// let et const
---
+++ js/1-variables/app2.js
@@ -0,0 +1,23 @@
+//Let et const
+// if (true) {
+//     let nom = 'Christian Lisangola'
+//     console.log('dans IF : ', nom)
+// }
+// console.log('En dehors de IF : ', nom)
+
+for (let i = 1; i <= 5; i = i + 1) {
+    console.log('i dans la boucle : ', i)
+}
+console.log('i en dehors de la boucle : ', i)
+
+// Concatenation
+alert('Je suis dans la joie')
+
+const noms = 'Christian Lisangola'
+
+const poste = 'Développeur'
+
+console.log(nom + ' est un ' + poste) //sans template string
+console.log(`${nom} est un ${post}`) //avec template string
+alert(nom + ' est un ' + poste) //sans template string
+alert(`${nom} est un ${post}`) //avec template string
