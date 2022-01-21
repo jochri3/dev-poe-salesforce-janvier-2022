568fbbcf87a459bb985046df470030015de4a9c7
Christian Lisangola <jochri3@gmail.com>
2022-01-21 18:51:37 +0300

    for et for..of

---
+++ js/3-loops/app.js
@@ -0,0 +1,25 @@
+// Syntaxe
+for (let i = 1; i <= 10; i++) {
+    console.log(i)
+}
+
+// Utilisation
+const name = 'Christian Lisangola'
+
+// for (let i = 0; i < name.length; i++) {
+//     console.log(`${i}  =>  ${name[i]}`) //name[0]
+// }
+
+//
+console.log('For classique')
+console.log('=============')
+for (let i = 0; i < name.length; i++) {
+    console.log(`${name[i]}`) //name[0]
+}
+
+// for...of
+console.log('For of')
+console.log('======')
+for (let caractere of name) {
+    console.log(caractere)
+}
